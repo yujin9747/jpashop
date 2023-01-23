f491c1add98e33039677b586b3239c2cd586eb79
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2023-01-23 23:44:06 +0900

    엔티티 클래스 설계1 완료 (#1)

---
+++ src/main/java/jpabook/jpashop/domain/Delevery.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delevery {
+    @Id @GeneratedValue
+    @Column(name = "delevery_id")
+    private Long id;
+    @OneToOne(mappedBy = "delivery")
+    private Order oder;
+    @Embedded
+    private Address address;
+    @Enumerated(EnumType.STRING)
+    private DeleveryStatus deleveryStatus;
+}
---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+    private int orderPrice; // 주문 당시 가격
+    private int count;  // 주문 당시 수량
+
+
+
+}
